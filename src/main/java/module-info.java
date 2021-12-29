f88c4c61f85d4710823dbb6abacc2c9c4c2d3a54
CrisVatca <vatcacristina@yahoo.com>
2021-12-29 16:11:48 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module ro.ubbcluj.map.map323ultima100m2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens ro.ubbcluj.map.map323ultima100m2 to javafx.fxml;
+    exports ro.ubbcluj.map.map323ultima100m2;
+}
